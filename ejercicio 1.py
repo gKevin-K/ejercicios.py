5129460036c0af904863b16bf1cd81871a2e50d4
Kevin M <kevinrojas5566@gmail.com>
2025-05-05 09:07:35 -0500

    Add files via upload

---
+++ ejercicio 1.py
@@ -0,0 +1,9 @@
+def media3(z:float, y:float, x:float):
+    z = float(z)
+    y = float(y)
+    x = float(x)
+    return(z + y + x) /3
+print(media3(5, 5, 5))
+print(media3(1, 3 , 8))
+print(media3(-1, 0, 7))
+print(media3(-3, 0 , 3))
