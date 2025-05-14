2e6ba58300b5b7cf65b78c33429e2691899b06e4
Kevin M <kevinrojas5566@gmail.com>
2025-05-14 14:27:40 -0500

    Add files via upload

---
+++ ejercicio7.py
@@ -0,0 +1,30 @@
+Entrance =input("Enter The Text: ")
+if Entrance.isdigit():
+    print("The argument Is Not str:")
+else:
+    print("The argument Not is int")
+    Words = Entrance.split(" ")
+    print(Words)
+    Counter = 0
+    WordsIndexOne = []
+    WordsMajorityOne = []
+    for Word in Words:
+        print(f"***{Word}***")
+    for Validator in Words :
+        if Word == Validator:
+            Counter +=1
+            print(f"-----{Word}---->{Counter}")
+    if Counter == 1: 
+        WordsIndexOne.append(Word)
+    if Counter > 1 and not Word in WordsMajorityOne :
+        WordsMajorityOne.append(Word)
+    Counter = 0
+    TextWithOutDuplicates = WordsIndexOne + WordsMajorityOne
+    FilledText = " ".join(TextWithOutDuplicates)
+    print(Words)
+    print(WordsIndexOne)
+    print(WordsMajorityOne)
+    print(TextWithOutDuplicates)
+    print(FilledText)
+
+
